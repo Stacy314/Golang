982d5c3377d077c808c5efc9c13735d6e0b56dc3
Stacy314 <167317438+Stacy314@users.noreply.github.com>
2024-07-25 18:56:57 +0000

    fix hw11 find_words

--- HW11/find_words.go
+++ HW11/find_words.go
@@ -1,8 +1,3 @@
-/*2. Implement a search for words with a certain pattern in a text file. Task: Create a regular 
-expression that can be used to find words that match a certain pattern. For example, an expression 
-that finds words that start with vowels and end with consonants, or words that consist of two 
-identical letters separated by any character. The task is creative.*/
-
 package main
 
 import (
@@ -14,6 +9,7 @@ import (
 )
 
 func main() {
+
     file, err := os.Open("text.txt")
     if err != nil {
         fmt.Println("Error opening file:", err)
@@ -22,39 +18,50 @@ func main() {
     defer file.Close()
 
     scanner := bufio.NewScanner(file)
-
+    var content strings.Builder
     for scanner.Scan() {
-        line := scanner.Text()
-        words := strings.Fields(line)
-
-        fmt.Println("Processing line:", line)
-
-        fmt.Println("Words consisting of two identical letters separated by any character:")
-        for _, word := range words {
-            if hasIdenticalLetters(cleanWord(word)) {
-                fmt.Println(cleanWord(word))
-            }
-        }
+        content.WriteString(scanner.Text() + " ")
     }
 
     if err := scanner.Err(); err != nil {
         fmt.Println("Error reading file:", err)
+        return
     }
-}
 
-func cleanWord(word string) string {
-    re := regexp.MustCompile(`[^\p{L}]`)
-    cleaned := re.ReplaceAllString(word, "")
-    return strings.ToLower(cleaned)
-}
+    wordPattern := `\b\w+\b`
+    re, err := regexp.Compile(wordPattern)
+    if err != nil {
+        fmt.Println("Error compiling regular expression:", err)
+        return
+    }
 
-func hasIdenticalLetters(word string) bool {
-    for i := 0; i < len(word)-1; i++ {
-        for j := i + 1; j < len(word); j++ {
-            if word[i] == word[j] {
-                return true
+    words := re.FindAllString(content.String(), -1)
+
+    checkPattern := func(word string) bool {
+        length := len(word)
+        for i := 0; i < length; i++ {
+            for j := i + 1; j < length; j++ {
+                if word[i] == word[j] {
+                    return true
+                }
             }
         }
+        return false
     }
-    return false
+
+    matchingWords := []string{}
+    for _, word := range words {
+        if checkPattern(word) {
+            matchingWords = append(matchingWords, word)
+        }
+    }
+
+    if len(matchingWords) > 0 {
+        fmt.Println("Found matching words:")
+        for _, match := range matchingWords {
+            fmt.Println(match)
+        }
+    } else {
+        fmt.Println("No matching words found.")
+    }
 }
